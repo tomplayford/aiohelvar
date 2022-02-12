cc9771c12cd726c93bec4dc7a30999110f5336dc
Tom Playford <tom@playford.net>
2022-02-12 14:34:43 +0000

    Better validation of Scene address objects

--- aiohelvar/parser/address.py
+++ aiohelvar/parser/address.py
@@ -3,31 +3,31 @@ class HelvarAddress:
     Represents a Helvar device address.
 
     Address format is @c.r.s.d
-    c - cluster: 0-253
+    c - block: 0-253
     r - router: 1-254
     s - subnet: 1-4
     d - device: 1-255
 
     TODO: validate the above.
 
-    incomplete addresses are possible, but must include at least cluster and router
+    incomplete addresses are possible, but must include at least block and router
 
     @0.1
 
-    cluster: 0
+    block: 0
     router: 1
 
     """
 
-    def __init__(self, cluster: int, router: int, subnet: int = None, device: int = None):
+    def __init__(self, block: int, router: int, subnet: int = None, device: int = None):
 
         self.subnet = subnet
         self.device = device
-        self.cluster = cluster
+        self.block = block
         self.router = router
 
     def __str__(self, separator="."):
-        base = f"@{self.cluster}{separator}{self.router}"
+        base = f"@{self.block}{separator}{self.router}"
         if self.subnet:
             base = f"{base}{separator}{self.subnet}"
         if self.device:
@@ -35,16 +35,16 @@ class HelvarAddress:
         return base
 
     @property
-    def cluster(self):
-        return self.__cluster
+    def block(self):
+        return self.__block
 
-    @cluster.setter
-    def cluster(self, var):
+    @block.setter
+    def block(self, var):
 
         var = int(var)
         if 0 > var > 253:
-            raise TypeError("Cluster must be between 1 and 4 or None.")
-        self.__cluster = var
+            raise TypeError("Block must be between 1 and 4.")
+        self.__block = var
 
     @property
     def router(self):
@@ -55,7 +55,7 @@ class HelvarAddress:
 
         var = int(var)
         if 1 > var > 254:
-            raise TypeError("Router must be between 1 and 4 or None.")
+            raise TypeError("Router must be between 1 and 4.")
         self.__router = var
 
     @property
@@ -96,7 +96,7 @@ class HelvarAddress:
 
     def __eq__(self, other):
 
-        for a in ("cluster", "router", "subnet", "device"):
+        for a in ("block", "router", "subnet", "device"):
 
             if getattr(self, a) is not None:
                 if getattr(other, a) is None:
@@ -109,7 +109,7 @@ class HelvarAddress:
         return True
 
     def __hash__(self):
-        return hash((int(self.cluster), int(self.router), int(self.subnet), self.device))
+        return hash((int(self.block), int(self.router), int(self.subnet), self.device))
 
     def __ne__(self, other):
         return not (self == other)
@@ -129,11 +129,48 @@ class SceneAddress:
     """
 
     def __init__(self, group: int, block: int, scene: int):
-        # TODO validate group, block and scene values are in range
 
-        self.group = int(group)
-        self.block = int(block)
-        self.scene = int(scene)
+        self.group = group
+        self.block = block
+        self.scene = scene
+
+    @property
+    def group(self):
+        return self.__group
+
+    @group.setter
+    def group(self, var):
+
+        var = int(var)
+        if 0 > var > 128:
+            raise TypeError("Group must be between 0 and 128.")
+        self.__group = var
+
+    @property
+    def block(self):
+        return self.__block
+
+    @block.setter
+    def block(self, var):
+
+        var = int(var)
+        if 1 > var > 8:
+            raise TypeError("Block must be between 1 and 8.")
+        self.__block = var
+
+
+    @property
+    def scene(self):
+        return self.__scene
+
+    @scene.setter
+    def scene(self, var):
+
+        var = int(var)
+        if 1 > var > 16:
+            raise TypeError("Scene must be between 1 and 6.")
+        self.__scene = var
+
 
     def __str__(self):
         return f"@{self.group}.{self.block}.{self.scene}"
