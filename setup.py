bd537fb188e07413fe13e459e155259c43e46404
Tom Playford <tom@playford.net>
2022-02-13 14:32:42 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aiohelvar",
-    version="0.9.2",
+    version="0.9.3",
     author="Tom Playford",
     description="Async HelvarNet communication and control library.",
     long_description=long_description,
